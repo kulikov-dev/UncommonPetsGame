4d53aafd02a51133443978490dc5a03f7f92d729
genki <reward.hunters@gmail.com>
2017-06-20 23:13:35 +0500

    * дикообраз

--- Assets/Scripts/SC_PointEnergy.cs
+++ Assets/Scripts/SC_PointEnergy.cs
@@ -1,17 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary> Точка с проводкой </summary>
-public class SC_PointEnergy : MonoBehaviour {
+public class SC_PointEnergy : MonoBehaviour
+{
+    /// <summary> Спрайт для работающей проводки </summary>
+    public Sprite Sprite_EnergyWork;
+    /// <summary> Спрайт для сломаной проводки </summary>
+    public Sprite Sprite_EnergyBroken;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool isLightBroken = false;
+    private bool IsLightBroken
+    {
+        get { return isLightBroken; }
+        set
+        {
+            isLightBroken = value;
+            transform.gameObject.GetComponent<SpriteRenderer>().sprite = value ? Sprite_EnergyBroken : Sprite_EnergyWork;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary> Проводка сломана </summary>
+    public void DestroyEnergy()
+    {
+        IsLightBroken = true;
+        // добавить: отмечает контроллеру дня и ночи, что свет в доме не работает
+    }
+
+    /// <summary> Попытаться починить проводку </summary>
+    public void TryToRepairEnergy()
+    {
+        //TODO запускает мини-игру, если она прошла успешно то отмечает что свет в доме работает
+        IsLightBroken = false;
+    }
 }
--- Assets/Scripts/SC_Porcupine.cs
+++ Assets/Scripts/SC_Porcupine.cs
@@ -1,31 +1,39 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
-public class SC_Porcupine : Animal,SC_ICleanable
+public class SC_Porcupine : Animal, SC_ICleanable
 {
-    /// <summary> Показывает, что дикообраз грызет проводку </summary>
-    public bool IsGnawing;
-
     private Coroutine C_DropNeedles;
     private Coroutine C_Gnawing;
 
     public GameObject P_Needles;
+
+    private float LastGnowingTime;
+    /// <summary> Время в течении которого точно не начнет грызть проводку </summary>
+    private float TimeBeforeNewGnowing = 10f;
+    /// <summary> Время в течении которого точно должен начать грызть проводку </summary>
+    private float NewGnowingTime = 30f;
+
+    /// <summary> Показывает, что дикообраз грызет проводку </summary>
+    private SC_PointEnergy currentPointEnergy;
 
     internal new void Start()
     {
         base.Start();
 
         StartDropNeedles();
+        LastGnowingTime = Time.time;
     }
 
     void SC_ICleanable.Clean()
     {
-        if (IsGnawing)
+        if (currentPointEnergy != null)
         {
             StopCoroutine(C_Gnawing);   // Остановить перегрызение проводки
-            IsGnawing = false;
+            currentPointEnergy = null;
 
             StartDropNeedles();     // начнем снова сбрасывать иголки, с горя.
             SelectNewTarget();      // отправим слоняться в другое место. 
@@ -35,10 +43,39 @@ public class SC_Porcupine : Animal,SC_ICleanable
     /// <summary> Запуск таймера сбрасывания иголок </summary>
     private void StartDropNeedles()
     {
-        StartCoroutine(TimerUpdate(UnityEngine.Random.Range(10, 50))); 
+        C_DropNeedles = StartCoroutine(DropNeedlesFunc(UnityEngine.Random.Range(5, 20)));
+    }
+    public override bool NeedSelectNewTarget(Transform oldTarget)
+    {
+        var newEnergyPoint = oldTarget.gameObject.GetComponent<SC_PointEnergy>();
+        if (newEnergyPoint != null)
+        {
+            Debug.Log("Start gnowing");
+            StopCoroutine(C_DropNeedles);
+            C_Gnawing= StartCoroutine(DestroyEnergyFunc(4));
+            LastGnowingTime = Time.time;
+            return false;
+        }
+        else
+        {
+            if (Level == 1 && UnityEngine.Random.value < Mathf.Min((Time.time - LastGnowingTime - TimeBeforeNewGnowing) / NewGnowingTime, 1.0f))
+            {
+                newEnergyPoint = FindObjectOfType<SC_PointEnergy>();
+                if (newEnergyPoint != null)
+                {
+                    Debug.Log("Go gnowing");
+                    currentPointEnergy = newEnergyPoint;
+                    SetTarget(newEnergyPoint.transform);
+                    return false;
+                }
+
+            }
+        }
+
+        return base.NeedSelectNewTarget(oldTarget);
     }
 
-    System.Collections.IEnumerator TimerUpdate(float duration)
+    IEnumerator DropNeedlesFunc(float duration)
     {
         while (true) // stopping condition?
         {
@@ -46,7 +83,19 @@ public class SC_Porcupine : Animal,SC_ICleanable
             yield return new WaitForSeconds(duration);
             Instantiate(P_Needles, transform.position, transform.rotation);
         }
-    
+
+    }
+    IEnumerator DestroyEnergyFunc(float duration)
+    {
+        while (true) // stopping condition?
+        {
+            yield return new WaitForSeconds(duration);
+            currentPointEnergy.DestroyEnergy();
+            StopCoroutine(C_Gnawing);
+            StartDropNeedles();
+            SelectNewTarget();
+        }
+
     }
 
 }
